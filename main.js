6ff96e4bbab61e3f8d271d023681511a6e120313
isc-garza <jorge.delagarza@intersystems.com>
2021-08-20 11:50:05 -0400

    Add .gitignore file; Pass in UMLS API key as parameter

--- main.js
+++ main.js
@@ -6,9 +6,21 @@ const cql = require('cql-execution');
 const cqlfhir = require('cql-exec-fhir');
 const cqlvsac = require('cql-exec-vsac');
 
-// JLD: Hardcode these for now
+// TO DO: There is supposed to be a way to set this within the environment so it
+//        doesn't have to be passed as an argument to the script. Investigate and
+//        document how to do this.
+const umlsApiKey = process.argv[2]
+if ((umlsApiKey==undefined)||(umlsApiKey==null)) {
+	let errorMsg = 'ERROR: No UMLS API key given; Script MUST be invoked with UMLS API key \r\n';
+	errorMsg += 'Ex:\r\n'
+	errorMsg += '\t\\isc-garza-cql-server>node main.js 98ffbd39-f414-48af-9f69-ae5de30d6c33'
+	console.error(errorMsg);
+	return;
+}
+// JLD: Hardcode this for now. In the future, it should probably be obtained from
+//      the request URL.
 const version = 'r4';
-const umlsApiKey = '39f22d5a-d804-4e9e-a90a-58b198822116';
+
 
 
 
@@ -38,16 +50,15 @@ const server = http.createServer(async (req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'application/json');
 
-	console.log('Request received:')
-	console.log('	Method: ' + req.method);
-	console.log('	URL: ' + req.url);
-	console.log('	Headers: ' + JSON.stringify(req.headers));
-	console.log();
+	console.log('\r\nRequest received:')
+	console.log('\tMethod: ' + req.method);
+	console.log('\tURL: ' + req.url);
+	console.log('\tHeaders: ' + JSON.stringify(req.headers));
 	let data = '';
 	for await (const chunk of req) {
 		data += chunk;
 	}
-	console.log(`Data received: \r\n${data}\r\n`);
+	console.log(`\tBody: ${data}\r\n`);
 	
 	const urlAry = req.url.split('/');
 	if (urlAry[1] == 'measure') {
@@ -88,20 +99,11 @@ const server = http.createServer(async (req, res) => {
 		res.end(returnBody);
 		return;
 	}
-  
-
-  
-  
-  
-  
-
-  
-  
-  
-  
-  
 });
 
+
+
+
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/ \r\nUsing UMLS API key: ${umlsApiKey}`);
 });
